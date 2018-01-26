116bd4041201ff904219497d1ca38a2bd61e904e
Unknown <fredrik.ridderheim@hotmail.com>
2018-01-26 11:34:35 +0100

    Added Queries in AccountController

--- Labb4/Labb4/controllers/AccountsController.cs
+++ Labb4/Labb4/controllers/AccountsController.cs
@@ -15,7 +15,37 @@ namespace Labb4.Controllers
         MongoContext Context = new MongoContext("Accounts");
         Accounts accounts = new Accounts();
 
-        public IEnumerable<Document> GetAllAccountDocuments()
+        public Document CreateOrUpdateDocumentById(string _Id, string username, string email, List<string> pictureId)
+        {
+            Document newDocument = new Document { Id = _Id, new username, new email, new pictureId };
+
+            
+
+            Document document = null;
+            try
+            {
+                document = (from doc in Context.Client.CreateDocumentQuery(Context.Database.SelfLink)
+                                where doc.Id == _Id
+                                select doc).FirstOrDefault();
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (document == null)
+                {
+                    Context.Collection..Add(score);
+                }
+                else
+                {
+                    return document;
+                }
+            }
+        }
+
+        public IEnumerable<Document> GetAllAccountsDocuments()
         {
            
 
@@ -23,7 +53,6 @@ namespace Labb4.Controllers
             {
                 var Documents = (from doc in Context.Client.CreateDocumentQuery(Context.Collection.SelfLink) select doc).AsEnumerable();
                 return Documents;
-                
             }
             catch (Exception)
             {
@@ -32,9 +61,31 @@ namespace Labb4.Controllers
             
         }
 
-        public Document GetAccountDocument()
+        public Document GetAccountDocumentById(string _id)
         {
-            var document = (from doc in Context.Client.createdoc)
+            try
+            {
+                var document = (from doc in Context.Client.CreateDocumentQuery(Context.Database.SelfLink)
+                                where doc.Id == _id
+                                select doc).FirstOrDefault();
+                return document;
+            }
+            catch (Exception e)
+            {
+                return null;
+                throw e;
+            }
+            
+        }
+
+        public Document UpdateAccountDocument(string _id)
+        {
+
+        }
+
+        public Document DeleteAccountDocument(string _id)
+        {
+
         }
     }
 }
