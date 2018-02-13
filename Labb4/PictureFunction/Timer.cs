8292dc269c0492050cc3fb49e429f082ae2f7752
Unknown <fredrik.ridderheim@hotmail.com>
2018-02-13 05:04:32 +0100

    Added Timer

---
+++ Labb4/PictureFunction/Timer.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using PictureFunction.Controllers;
+
+namespace PictureFunction
+{
+    public static class Timer
+    {
+        [FunctionName("Timer")]
+        public static void Run([TimerTrigger("0 */20 * * * *")]TimerInfo myTimer, TraceWriter log)
+        {
+            log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+            PictureController pictureController = new PictureController();
+
+            var pictures = pictureController.GetAllPictures();
+            foreach(var picture in pictures)
+            {
+                if(picture.Valid == false && picture.PictureName.EndsWith(".png"))
+                {
+                    pictureController.UpdatePictureDocument(picture.PictureName, true);
+                }
+            }
+        }
+    }
+}
